2d7fda4d10bd40a57c6e33bb7030676e7924ef27
osaka0813-heng <osaka0813@gmail.com>
2026-09-05 10:24:03 +0900

    Serve a lightweight published content bundle

--- src/config/remoteContent.ts
+++ src/config/remoteContent.ts
@@ -8,8 +8,8 @@
  * https://raw.githubusercontent.com/<user>/<repo>/main/remote-content.json
  */
 export const REMOTE_CONTENT_URL =
-  'https://raw.githubusercontent.com/osaka0813-heng/insight-build-front-deepseek-ver-/main/remote-content.json';
+  'https://raw.githubusercontent.com/osaka0813-heng/insight-build-front-deepseek-ver-/main/published-content.json';
 
 export const REMOTE_CONTENT_FETCH_TIMEOUT_MS = 8_000;
 export const REMOTE_CONTENT_MIN_REFRESH_INTERVAL_MS = 0;
-export const REMOTE_CONTENT_CACHE_KEY = '@insight/remote-content/v2';
+export const REMOTE_CONTENT_CACHE_KEY = '@insight/published-content/v1';
